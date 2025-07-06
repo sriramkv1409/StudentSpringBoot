b592bde7dd0f152569df1091565413e8ea1befc5
sriramkv1409 <sriramkv1409@gmail.com>
2025-07-06 11:35:21 +0530

    Performed CRUD operations

--- src/main/java/com/example/Student/Controllers/StudentController.java
+++ src/main/java/com/example/Student/Controllers/StudentController.java
@@ -3,27 +3,42 @@ package com.example.Student.Controllers;
 import com.example.Student.Models.StudentModel;
 import com.example.Student.Services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/Student")
 public class StudentController {
   //Service Layer
     @Autowired
     StudentService ss;
 
-    @PostMapping("/")
+    @PostMapping
     public String addStudent(@RequestBody StudentModel s){
         ss.addStudent(s);
         return "Student Added Successfully";
     }
-    @GetMapping("/")
+
+
+    @GetMapping
     public List<StudentModel> getStudents(){
         return ss.getAllStudents();
+    }
+
+    @GetMapping("/{id}")
+    public StudentModel getStudentById(@PathVariable int id){
+        return ss.getStudentById(id);
+    }
+
+    @PutMapping("/{id}")
+    public String updateStudent(@RequestBody StudentModel s){
+        return ss.updateStudent(s);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteStudent(@PathVariable int id){
+        return ss.deleteStudentById(id);
     }
 
 }
--- src/main/java/com/example/Student/Services/StudentService.java
+++ src/main/java/com/example/Student/Services/StudentService.java
@@ -21,4 +21,52 @@ public class StudentService {
         return student;
     }
 
+
+    public StudentModel getStudentById(int id) {
+        for(StudentModel s:student){
+            if(s.getId() == id){
+                return s;
+            }
+        }
+        return null;
+    }
+
+
+    public String updateStudent(StudentModel s) {
+        int ind = 0;
+        boolean flag = false;
+        for(int i=0;i<student.size();i++){
+            if(s.getId() == student.get(i).getId()){
+                ind = i;
+                flag = true;
+                break;
+            }
+        }
+        if(flag){
+            student.set(ind,s);
+            return "Updated Student Successfully";
+        }
+        else{
+            return "Student not found";
+        }
+    }
+
+    public String deleteStudentById(int id) {
+        int ind = 0;
+        boolean flag = false;
+        for(int i=0;i<student.size();i++){
+            if(id == student.get(i).getId()){
+                ind = i;
+                flag = true;
+            }
+        }
+        if(flag){
+            student.remove(ind);
+            return "Student deleted Succesfully";
+        }
+        else{
+            return "Student not found";
+        }
+
+    }
 }
